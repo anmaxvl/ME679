d4d897fde4a46e5076e197963c3f17ef8dd3b887
sergey <sergey@iMac-06ui-iMac.local>
2013-11-28 14:51:12 +0900

    Initial Commit

---
+++ iLaunchPad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iLaunchPad
+//
+//  Created by sergey on 11/28/13.
+//  Copyright (c) 2013 GIST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iLaunchPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iLaunchPad
+//
+//  Created by sergey on 11/28/13.
+//  Copyright (c) 2013 GIST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
